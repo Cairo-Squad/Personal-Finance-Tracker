5973920ee689dc6e125e58641c583ea4a3d63ac3
Nourhan Adel <norhanadel367@gmail.com>
2025-04-09 18:43:02 +0200

    feat:create category manager test, Category manager

---
+++ src/util/CategoryManager.kt
@@ -0,0 +1,26 @@
+package util
+
+import model.Category
+
+object CategoryManager {
+    fun checkCategoryManager(choose:Int,categories: MutableList<Category>){
+
+    }
+
+
+    fun checkCategorySelection(
+        categories: MutableList<Category>,
+        userInput: String
+    ): Boolean {
+       return true
+    }
+
+    fun checkNewCategoryName(
+        categories: MutableList<Category>,
+        userInput: String
+    ): Boolean {
+
+        return true
+    }
+
+}
