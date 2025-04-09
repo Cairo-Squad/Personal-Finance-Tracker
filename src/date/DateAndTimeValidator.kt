1d4d595649ddbbabeb2c06d73ce7fd8e3e965b9b
mohamed_Galal <az.galal666@gmail.com>
2025-04-09 18:31:38 +0200

    feature: add isTimeValid function in DateAndTime Validator class

--- src/date/DateAndTimeValidator.kt
+++ src/date/DateAndTimeValidator.kt
@@ -1,6 +1,7 @@
 package date
 
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
@@ -16,6 +17,18 @@ object DateAndTimeValidator {
             val parsedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern))
             parsedDate.year in minYear..maxYear
 
+        } catch (e: DateTimeParseException) {
+            false
+        }
+    }
+
+    fun isTimeValid(
+        time: String,
+        pattern: String = "HH:mm",
+    ): Boolean {
+        return try {
+            LocalTime.parse(time, DateTimeFormatter.ofPattern(pattern))
+            true
         } catch (e: DateTimeParseException) {
             false
         }
