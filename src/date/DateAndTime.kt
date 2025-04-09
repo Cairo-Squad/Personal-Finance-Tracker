8add5ccdc11d2eb2ea1e386579935509be4304cb
mohamed_Galal <az.galal666@gmail.com>
2025-04-09 09:55:15 +0200

    feature: add date and time data class

---
+++ src/date/DateAndTime.kt
@@ -0,0 +1,25 @@
+package date
+
+import java.time.LocalDateTime
+
+data class DateAndTime(
+    private var date: String,
+    private var time: String
+) {
+
+    fun setDate(year: Int, month: Int, day: Int) {
+        date = "$day-$month-$year"
+    }
+
+    fun setTime(hour: Int, minute: Int) {
+        time = "$hour:$minute"
+    }
+
+    fun getDate(): String {
+        return date
+    }
+
+    fun getTime(): String {
+        return time
+    }
+}
