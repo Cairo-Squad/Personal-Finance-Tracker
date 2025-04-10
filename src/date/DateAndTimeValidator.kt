afb226704b06e8a27c5d9b950ad91005be4483dc
mohamed_Galal <az.galal666@gmail.com>
2025-04-10 13:58:25 +0200

    feature: add getLocalDate and getLocalDateTime helper function

--- src/date/DateAndTimeValidator.kt
+++ src/date/DateAndTimeValidator.kt
@@ -1,37 +1,6 @@
 package date
 
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
-
-object DateAndTimeValidator {
-
-    fun isDateValid(
-        date: String,
-        pattern: String = "dd-MM-yyyy",
-        minYear: Int = 2020,
-        maxYear: Int = 2030
-    ): Boolean {
-        return try {
-            val parsedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern))
-            parsedDate.year in minYear..maxYear
-
-        } catch (e: DateTimeParseException) {
-            false
-        }
-    }
-
-    fun isTimeValid(
-        time: String,
-        pattern: String = "HH:mm",
-    ): Boolean {
-        return try {
-            LocalTime.parse(time, DateTimeFormatter.ofPattern(pattern))
-            true
-        } catch (e: DateTimeParseException) {
-            false
-        }
-    }
-
+fun isYearValid(year:Int): Boolean {
+    println(year > 2030 || year < 2025)
+    return year in 2025..2030
 }
