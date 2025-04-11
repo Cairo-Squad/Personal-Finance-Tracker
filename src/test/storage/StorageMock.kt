108d5386629eb20d32116551b12cf2dbc41855e2
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-11 19:17:45 +0200

    refactor: implement the logic of crud operations

--- src/test/storage/StorageMock.kt
+++ src/test/storage/StorageMock.kt
@@ -1,20 +1,38 @@
 package test.storage
 
 import datasource.storage.MemoryStorage
+import datasource.storage.MemoryStorageImp
 import model.Transaction
 import java.time.LocalDate
 
 class StorageMock(private val transactionList: MutableList<Transaction>): MemoryStorage {
     override fun addTransaction(transaction: Transaction): Boolean {
-        TODO("Not yet implemented")
+        transactionList.add(transaction)
+        return true
     }
 
     override fun updateTransaction(transaction: Transaction):Boolean {
-        TODO("Not yet implemented")
+        val index = transactionList.indexOfFirst { it.transactionId == transaction.transactionId }
+        if (index != -1) {
+            val oldTransaction = transactionList[index]
+            val updatedTransaction = transaction.copy(
+                transactionDescription = transaction.transactionDescription ?: oldTransaction.transactionDescription,
+                transactionType = transaction.transactionType ?: oldTransaction.transactionType,
+                transactionAmount = transaction.transactionAmount ?: oldTransaction.transactionAmount,
+                transactionDate = transaction.transactionDate ?: oldTransaction.transactionDate,
+                transactionCategory = transaction.transactionCategory ?: oldTransaction.transactionCategory
+            )
+            transactionList[index] = updatedTransaction
+            return true
+        }
+        return false
     }
 
     override fun deleteTransaction(transactionId: Int) {
-        TODO("Not yet implemented")
+        val index = transactionList.indexOfFirst { it.transactionId == transactionId }
+        if (index != -1) {
+            transactionList.removeAt(index)
+        }
     }
 
     override fun getTransactionById(transactionId: Int): Transaction? {
@@ -26,10 +44,12 @@ class StorageMock(private val transactionList: MutableList<Transaction>): Memory
     }
 
     override fun getReportByMonth(localDate : LocalDate): List<Transaction> {
-        TODO("Not yet implemented")
+        return transactionList.filter { transaction ->
+            transaction.transactionDate?.year == localDate.year && transaction.transactionDate?.monthValue == localDate.monthValue
+        }
     }
 
     override fun getNewTransactionId(): Int {
-        TODO("Not yet implemented")
+        return transactionList.size + 1
     }
 }
