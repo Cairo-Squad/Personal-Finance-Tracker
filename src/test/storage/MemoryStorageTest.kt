267cb31f7da634909d62fc3b634a4a5ca188b7df
ahmed samy <ahmbdelkhalek@gmail.com>
2025-04-11 07:16:19 +0200

    refactor: make a function that have all the test cases of getTransactionById and getAllTransactions

--- src/test/storage/MemoryStorageTest.kt
+++ src/test/storage/MemoryStorageTest.kt
@@ -1,47 +1,22 @@
+package test.storage
+
 import datasource.MemoryStorage
 import datasource.Storage
 import model.Category
 import model.Transaction
 import model.TransactionType
-import test.storage.StorageMock
 import test.util.test
 import java.time.LocalDateTime
-
-//package test.storage
-//fun main(){
-//    // valid id test
-//    check(
-//        name = "given valid id , when checked, then should return true",
-//        result = checkID(id : Int),
-//        correctResult = true
-//    )
-//    //empty id test
-//    check(
-//        name = "given empty id , when checked, then should return false",
-//        result =checkID(id :Int),
-//        correctResult = false
-//    )
-//    //out of range id
-//    check(
-//        name = "given out of range id , when checked, then should return false",
-//        result =checkID(id : Int),
-//        correctResult = false
-//    )
-//
-//
-//
-//}
-//fun check(name: String, result:Boolean , correctResult:Boolean) {
-//    if (result == correctResult) {
-//        println("Success - $name")
-//    } else {
-//        println("Failed - $name")
-//    }
-//}
 
 
 fun main() {
 
+  runCheckGetTransactions()
+
+
+}
+
+fun runCheckGetTransactions(){
     //region getTransactionById()
     // Empty storage
     run {
@@ -232,8 +207,6 @@ fun main() {
 
     //return a non-empty list
     //endregion
-
-
 }
 
 class TransactionManagerMock(
